ff0280a80bb6f57ce79751d5899e07a6a5da93c5
Victor Manoliu <victormanoliu97@gmail.com>
2018-11-14 02:00:20 +0200

    Added the Business Layer and implemented the city controller

---
+++ Laborator 7/BusinessLayer/Repository/ICityRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using DataLayer.CityData;
+
+namespace BusinessLayer.Repository
+{
+    public interface ICityRepository
+    {
+        void CreateCity(City city);
+        void RemoveCity(int id);
+        void EditCity(int id, City city);
+        City GetCity(int id);
+        List<City> GetCities();
+    }
+}
--- Laborator 7/Laborator 7/Startup.cs
+++ Laborator 7/Laborator 7/Startup.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BusinessLayer.Repository;
+using DataLayer.Management;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace Laborator_7
 {
@@ -24,6 +21,11 @@ namespace Laborator_7
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationContext>(Options =>
+                Options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Lab06DB;Trusted_Connection=True;"));
+            services.AddTransient<ICityRepository, CityRepository>();
+            services.AddTransient<IPoiRepository, PoiRepository>();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
